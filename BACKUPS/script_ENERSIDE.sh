eee31cbb42a6060af335d8f98b1f06206e64e59e
jaorobertosz <joao219roberto@gmail.com>
2024-02-16 17:33:25 -0300

    Atualização 16/02

---
+++ BACKUPS/script_ENERSIDE.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SRV=$(hostname)
+DATA=$(date +%d%m%Y%H%M)
+FILE="BKP_$SRV_$DATA.tgz"
+CLIENTE='ENERSIDE'
+BKPETH1_IT='172.27.1.251'
+BKPETH2_IT='172.27.1.252'
+BKPETH1_RS='172.27.2.251'
+BKPETH2_RS='172.27.2.252'
+USERZBX='suporteati'
+BKPDIRUTR='/root/BACKUP/BKP_*'
+BKPDIR='/USINAS/'$CLIENTE'/BACKUP/'
+MSG1="SUCESSO NA COLETA DE BACKUP"
+MSG2="FALHA AO GERAR BACKUP"
+MSG3="BACKUP COLETADO E COPIADO PARA ATI COM SUCESSO"
+MSG4="FALHA NA TRANSFERENCIA DO BACKUP PARA A ATI"
+MSG5="ALTERAÇÃO DE PERMISSÃO REALIZADA"
+
+# Remover o backup anterior
+if [ -e "$BKPDIR" ]; then
+  rm -f "$BKPDIR"*
+fi
+
+#	BACKUP ITABIRA_ETH1
+	scp "$BKPETH1_IT:$BKPDIRUTR" "$BKPDIR" && echo "$(date +%d%m%Y%H%M) - $MSG1" && echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG3" || echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG4" && chown "$USERZBX". "$BKPDIR" -R && echo "$DATA - $MSG5" 
+
+#	BACKUP ITABIRA_ETH2
+	scp "$BKPETH2_IT:$BKPDIRUTR" "$BKPDIR" && echo "$(date +%d%m%Y%H%M) - $MSG1" && echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG3" || echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG4" && chown "$USERZBX". "$BKPDIR" -R && echo "$DATA - $MSG5" 
+
+#	BACKUP ROTA_DO_SOL_ETH1
+	scp "$BKPETH1_RS:$BKPDIRUTR" "$BKPDIR" && echo "$(date +%d%m%Y%H%M) - $MSG1" && echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG3" || echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG4" && chown "$USERZBX". "$BKPDIR" -R && echo "$DATA - $MSG5"
+
+#	BACKUP ROTA_DO_SOL_ETH2
+	scp "$BKPETH2_RS:$BKPDIRUTR" "$BKPDIR" && echo "$(date +%d%m%Y%H%M) - $MSG1" && echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG3" || echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG4" && chown "$USERZBX". "$BKPDIR" -R && echo "$DATA - $MSG5"
