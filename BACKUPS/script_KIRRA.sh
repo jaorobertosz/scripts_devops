e6ff221785380c94e9340ea3e33e9f7ffed77fcd
jaorobertosz <joao219roberto@gmail.com>
2024-02-16 12:01:57 -0300

    Atualização 16/02

--- BACKUPS/script_KIRRA.sh
+++ BACKUPS/script_KIRRA.sh
@@ -3,11 +3,13 @@
 SRV=$(hostname)
 DATA=$(date +%d%m%Y%H%M)
 FILE="BKP_$SRV_$DATA.tgz"
-BKPETH1='172.28.4.251'
-BKPETH2='172.28.4.252'
+CLIENTE='KIRRA'
+USINA='KIRRA'
+BKPETH1='172.33.1.251'
+BKPETH2='172.33.1.252'
 USERZBX='suporteati'
 BKPDIRUTR='/root/BACKUP/BKP_*'
-BKPDIR='/USINAS/CARVALHO-ENERGIAS/BACKUP'
+BKPDIR='/USINAS/'$CLIENTE'/'$USINA'/BACKUP/'
 MSG1="SUCESSO NA COLETA DE BACKUP"
 MSG2="FALHA AO GERAR BACKUP"
 MSG3="BACKUP COLETADO E COPIADO PARA ATI COM SUCESSO"
@@ -23,4 +25,4 @@ fi
 	scp "$BKPETH1:$BKPDIRUTR" "$BKPDIR" && echo "$(date +%d%m%Y%H%M) - $MSG1" && echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG3" || echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG4" && chown "$USERZBX". "$BKPDIR" -R && echo "$DATA - $MSG5" 
 
 #	BACKUP MATHEUS-LEME_ETH2
-	scp "$BKPETH2:$BKPDIRUTR" "$BKPDIR" && echo "$(date +%d%m%Y%H%M) - $MSG1" && echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG3" || echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG4" 
+	scp "$BKPETH2:$BKPDIRUTR" "$BKPDIR" && echo "$(date +%d%m%Y%H%M) - $MSG1" && echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG3" || echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG4" && chown "$USERZBX". "$BKPDIR" -R && echo "$DATA - $MSG5" 
