bad8b8c125d1b79db7dc675d0486d009a745ff67
jaorobertosz <joao219roberto@gmail.com>
2024-02-19 10:27:51 -0300

    Commit Atualização 19/02

--- USINAS/verifica_backup_ENERSIDE.sh
+++ USINAS/verifica_backup_ENERSIDE.sh
@@ -3,20 +3,29 @@
 CLIENTE="ENERSIDE" # Substitua "nome_do_cliente" pelo nome real do cliente
 DIRETORIO="/USINAS/$CLIENTE/BACKUP/"
 
-# Obtém a data da última segunda-feira
-data_segunda=$(date -d "last Monday" +%Y-%m-%d)
+# Data atual
+data_atual=$(date +%Y-%m-%d)
+
+# Verifica se a data atual é uma segunda-feira
+if [ $(date +%u) -eq 1 ]; then
+    data_verificacao=$data_atual
+else
+    # Obtém a data da última segunda-feira
+    data_verificacao=$(date -d "last Monday" +%Y-%m-%d)
+fi
 
 # Verifica se o diretório existe
 if [ -d "$DIRETORIO" ]; then
-    # Lista backups (tar.gz) modificados na última segunda-feira
-    backups=$(find "$DIRETORIO" -name "*.tar.gz" -newermt "$data_segunda" ! -newermt "$data_segunda +1 week" -exec stat --format="%n - %y" {} +)
+    # Lista backups (tar.gz) modificados na segunda-feira atual
+    backups=$(find "$DIRETORIO" -name "*.tgz" -newermt "$data_verificacao" ! -newermt "$data_verificacao +1 day" -exec stat --format="%n - %y" {} +)
 
     if [ -n "$backups" ]; then
-        echo "Backups encontrados para o cliente $CLIENTE na última segunda-feira ($data_segunda):"
+        echo "Backups encontrados para o cliente $CLIENTE na segunda-feira ($data_verificacao):"
         echo "$backups"
     else
-        echo "Nenhum backup encontrado para o cliente $CLIENTE na última segunda-feira ($data_segunda)."
+        echo "Nenhum backup encontrado para o cliente $CLIENTE na segunda-feira ($data_verificacao)."
     fi
 else
     echo "O diretório $DIRETORIO não existe."
 fi
+
