d11fca23021ab4dbec3f13549f5f62446615bb2a
jaorobertosz <joao219roberto@gmail.com>
2024-02-29 17:30:12 -0300

    feat: adições de backup TRINITY e TESTE's

---
+++ BACKUPS/script_TRINITY.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+SRV=$(hostname)
+DATA=$(date +%d%m%Y%H%M)
+CLIENTE='TRINITY'
+FILE="BKP_$SRV_$DATA.tgz"
+BKPML1_ETH1='172.26.1.251'
+BKPML1_ETH2='172.26.1.252'
+BKPVSSR_ETH1='172.31.200.31'
+BKPVSSR_ETH2='172.31.200.32'
+USERZBX='suporteati'
+BKPDIRUTR=/root/BACKUP/BKP_*
+BKPDIR=/USINAS/${CLIENTE}/BACKUP/
+MSG1="SUCESSO NA COLETA DE BACKUP"
+MSG2="FALHA AO GERAR BACKUP"
+MSG3="BACKUP COLETADO E COPIADO PARA ATI COM SUCESSO"
+MSG4="FALHA NA TRANSFERENCIA DO BACKUP PARA A ATI"
+MSG5="ALTERAÇÃO DE PERMISSÃO REALIZADA"
+
+# Remover o backup anterior
+if [ -e "$BKPDIR" ]; then
+  rm -f "$BKPDIR"*
+fi
+
+#	BACKUP ETH1
+	
+	echo "Realizando BACKUP ETH1"
+	sleep 5s
+	scp "$BKPML1_ETH1:$BKPDIRUTR" "$BKPDIR" && echo "$(date +%d%m%Y%H%M) - $MSG1" && echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG3" || echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG4" && chown "$USERZBX". "$BKPDIR" -R && echo "$DATA - $MSG5"
+
+	sleep 5s
+	scp "$BKPVSSR_ETH1:$BKPDIRUTR" "$BKPDIR" && echo "$(date +%d%m%Y%H%M) - $MSG1" && echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG3" || echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG4" && chown "$USERZBX". "$BKPDIR" -R && echo "$DATA - $MSG5"	
+
+#	BACKUP ETH2
+
+	echo "Realizando BACKUP ETH2"
+	sleep 5s
+	scp "$BKPML1_ETH2:$BKPDIRUTR" "$BKPDIR" && echo "$(date +%d%m%Y%H%M) - $MSG1" && echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG3" || echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG4"
+
+	sleep 5s
+	scp "$BKPVSSR_ETH2:$BKPDIRUTR" "$BKPDIR" && echo "$(date +%d%m%Y%H%M) - $MSG1" && echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG3" || echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG4" && chown "$USERZBX". "$BKPDIR" -R && echo "$DATA - $MSG5" 	
---
+++ USINAS/verifica_backup_TESTE.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+CLIENTE="CLIENTE" # Substitua "nome_do_cliente" pelo nome real do cliente
+DIRETORIO="/USINAS/$CLIENTE/BACKUP/"
+
+# Data atual
+data_atual=$(date +%Y-%m-%d)
+
+# Verifica se a data atual é uma segunda-feira
+if [ $(date +%u) -eq 1 ]; then
+    data_verificacao=$data_atual
+else
+    # Obtém a data da última segunda-feira
+    data_verificacao=$(date -d "last Monday" +%Y-%m-%d)
+fi
+
+# Verifica se o diretório existe
+if [ -d "$DIRETORIO" ]; then
+    # Lista backups (tar.gz) modificados na segunda-feira atual
+    backups=$(find "$DIRETORIO" -name "*.tgz" -newermt "$data_verificacao" ! -newermt "$data_verificacao +1 day" -exec stat --format="%n - %y" {} +)
+
+    if [ -n "$backups" ]; then
+        echo "Backups encontrados para o cliente $CLIENTE na segunda-feira ($data_verificacao):"
+        echo "$backups"
+    else
+        echo "Nenhum backup encontrado para o cliente $CLIENTE na segunda-feira ($data_verificacao)."
+    fi
+else
+    echo "O diretório $DIRETORIO não existe."
+fi
+
