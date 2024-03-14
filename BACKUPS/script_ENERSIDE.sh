775fc10677706f066f798526cd4873baca210bea
jaorobertosz <joao219roberto@gmail.com>
2024-03-14 10:02:16 -0300

    feat: modificação do BACKUPS

--- BACKUPS/script_ENERSIDE.sh
+++ BACKUPS/script_ENERSIDE.sh
@@ -23,7 +23,8 @@ MSG5="ALTERAÇÃO DE PERMISSÃO REALIZADA"
 #fi
 
 #	BACKUP ETH1
-	
+	echo "Realizando BACKUP ETH1"
+
 	sleep 5s
 	scp -p -qo ConnectTimeout=3 "$BKPETH1_IT:$BKPDIRUTR" "$BKPDIR" && echo "$(date +%d%m%Y%H%M) - $MSG1" && echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG3" || echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG4" && chown "$USERZBX". "$BKPDIR" -R && echo "$DATA - $MSG5" 
 
@@ -31,6 +32,7 @@ MSG5="ALTERAÇÃO DE PERMISSÃO REALIZADA"
 	scp -p -qo ConnectTimeout=3 "$BKPETH1_RS:$BKPDIRUTR" "$BKPDIR" && echo "$(date +%d%m%Y%H%M) - $MSG1" && echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG3" || echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG4" && chown "$USERZBX". "$BKPDIR" -R && echo "$DATA - $MSG5"
 
 #	BACKUP ITABIRA_ETH2
+	echo "Realizando BACKUP ETH2"
 
 	sleep 5s 
 	scp -p -qo ConnectTimeout=3 "$BKPETH2_IT:$BKPDIRUTR" "$BKPDIR" && echo "$(date +%d%m%Y%H%M) - $MSG1" && echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG3" || echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG4" && chown "$USERZBX". "$BKPDIR" -R && echo "$DATA - $MSG5" 
