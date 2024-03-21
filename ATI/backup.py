21cadf550838b8fd7491205382c2a263562eb136
jaorobertosz <joao219roberto@gmail.com>
2024-03-21 17:22:53 -0300

    Initial commit

--- ATI/backup.py
+++ ATI/backup.py
@@ -1,10 +1,6 @@
 import os
 import paramiko
 import random
-import subprocess
-
-def run_command(command):
-    subprocess.run(command, shell=True, check=True)
 
 def backup_local_directory(local_dir, server_address, username, password, local_directory):
     # Verifica se o diretório local existe
@@ -22,19 +18,9 @@ def backup_local_directory(local_dir, server_address, username, password, local_
     SRV=$(hostname)
     DATA=$(date +%d%m%Y%H%M)
     FILE=BKP_"$SRV"_"$DATA".tgz
-    MSG1="SUCESSO GERACAO DO BACKUP $FILE"
-    MSG2="FALHA AO GERAR BACKUP"
-    MSG3="BACKUP $FILE GERADO E COPIADO PARA ATI COM SUCESSO"
-    MSG4="FALHA NA TRANSFERENCIA DO BACKUP PARA A ATI"
-    BKPDIR="/root/BACKUP/"
-
-    # Remover o backup anterior
-    if [ -e "$BKPDIR" ]; then
-      rm -f "$BKPDIR"*
-    fi
 
     # Gera arquivo de backup compactado
-    tar --exclude='*.*~' --exclude='*.tgz' --exclude='*.log' -czf "$BKPDIR$FILE" /etc/network/interfaces /etc/hosts /etc/resolv.conf /etc/zabbix /ATI/sgd >/dev/null 2>&1 && echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG1" || echo "$(date +%d/%m/%Y-%H:%M:%S) - $MSG2"
+    tar --exclude='*.*~' --exclude='*.tgz' --exclude='*.log' -czf "$FILE" /etc/network/interfaces /etc/hosts /etc/resolv.conf /etc/zabbix /ATI/sgd >/dev/null 
     """
 
     # Executa o comando remoto
@@ -42,15 +28,18 @@ def backup_local_directory(local_dir, server_address, username, password, local_
     print(stdout.read().decode())
     print(stderr.read().decode())
 
-    # Transferência do arquivo gerado para o diretório local
-    sftp = ssh.open_sftp()
-    remote_file = "/root/BACKUP/" + cmd.split("$FILE")[1].split(" ")[0]
-    local_file = os.path.join(local_directory, os.path.basename(remote_file))
-    sftp.get(remote_file, local_file)
-    sftp.close()
+    # Obtem o nome do arquivo remoto
+    remote_file = cmd.split("$FILE")[1].split('"')[1].strip()
     
-    print(f"Backup transferido para {local_file}")
-
+    # Tenta fazer o download do arquivo
+    try:
+        sftp = ssh.open_sftp()
+        sftp.get( remote_file, os.path.join(local_directory, remote_file))
+        print(f"Backup transferido para {os.path.join(local_directory, remote_file)}")
+        sftp.close()
+    except FileNotFoundError:
+        print(f"Arquivo remoto '{remote_file}' não encontrado.")
+    
     ssh.close()
 
 def select_server_address_from_file(file_path):
@@ -63,12 +52,11 @@ def select_server_address_from_file(file_path):
         return None
 
 # Configurações
-local_directory = "/home/souza/BACKUP"
-username = "sysadm"
-password = "$3cur1ty"
+local_directory = "/USINAS/TESTE/BACKUP"
+username = "root"
+password = ""
 ips_file_path = "ips.txt"  # Caminho para o arquivo de IPs
 
-# Seleciona um IP aleatório do arquivo
 server_address = select_server_address_from_file(ips_file_path)
 if server_address:
     # Chamada da função para fazer o backup
